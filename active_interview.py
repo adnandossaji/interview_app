466eb4b90b3ba9a4ea5a34c7ef030127f38aab2e
codycas <codypatcastino@gmail.com>
2016-11-15 12:40:04 -0600

    str conversion error fix

--- active_interview.py
+++ active_interview.py
@@ -5,7 +5,7 @@ import interview
 
 
 # an object that consists of an Interview object and a dictionary consisting of key value pair's, (QuestionNumber,Question)
-# object can store and return a list of Answers, Answers can be stored one at a time with putAnswer()
+# object can store and return a list of Answers, Answers can be stored one at a time with putAnswer() 
 # a list of questions may be submitted during the construction of the object or added one at a time after construction
 class ActiveInterview():
 	def __init__(self,InterviewID,InterviewName, Questions=None):
@@ -14,37 +14,38 @@ class ActiveInterview():
 		self.questions = [] if Questions is None else Questions
 		self.iter = 0
 		self.InterviewLength =len(self.questions)
-
+		
 	def putQuestion(self, question):
-		self.questions[question.getNumQ] = question
-
+		self.questions.add(question)
+		
 	def getInterviewID(self):
 		return self.interviewID
-
+		
 	def getInterviewName(self):
 		return self.interviewName
-
+		
 	def getQuestions(self):
 		return self.questions
-
+		
 	def getQuestion(self):
 		return self.questions[self.iter]
-
+	
 	def answerQuestion(self, AnswerID):
 		self.questions[self.iter].answerQuestion(AnswerID)
-
-	# returns the current question and increments the curQ
+	
+	# returns the current question and increments the iter
 	def getNextQuestion(self):
-
-		if self.iter ==self.InterviewLength:
+		if self.iter == self.InterviewLength:
 			return 'End of Interview'
 		else:
 			res = self.questions[self.iter]
 			self.iter = self.iter + 1
 			return res
-
+		
 	def __str__(self):
 		ret = '\n'
 		for question in self.questions:
 			ret = ret + '\t' + str(question) + '\n'
-		return '{ InterviewID: ' + self.interviewID + ' InterviewName: ' + self.interviewName + ret + '}'
+		return '{ InterviewID: ' + str(self.interviewID) + ' InterviewName: ' + str(self.interviewName) + ret + '}'
+		
+	
