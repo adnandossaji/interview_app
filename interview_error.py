b84257ad067eefbb6d3854e52e1058c09afa578f
akhan8904 <akhan8904@gmail.com>
2016-11-18 21:54:18 -0600

    Update interview_error.py

--- interview_error.py
+++ interview_error.py
@@ -2,3 +2,12 @@ class CredentialsException(Exception):
     def __init__(self,*args,**kwargs):
         Exception.__init__(self,"Username or password incorrect.",**kwargs)
         print("Username or password incorrect.")
+        
+    def startInterview(self, InterviewID):
+        try:
+            self.Interview = db_interaction.getInterview(self.InterviwID)
+        except:
+            print('Sorry No Interview Found')
+            raise CredentialsException('No Ineterview Found!')
+        else:
+            print('Now Starting the Interview')
